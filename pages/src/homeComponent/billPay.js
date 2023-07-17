4997a099a98c5e289371c77192e7f4525f9c4855
Sunny4943 <85888495+Sunny4943@users.noreply.github.com>
2023-07-17 15:01:18 +0530

    Update billPay.js

--- pages/src/homeComponent/billPay.js
+++ pages/src/homeComponent/billPay.js
@@ -1,1 +1,39 @@
 
+import React from "react";
+class BillPay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onHandleChange.bind(this);
+        this.onHandleButtonClick.bind(this);
+        // this.onModalClick.bind(this);
+    }
+
+    onHandleChange = (name, value) => {
+        this.props.homeCallback(name, value);
+    };
+    onClientChange = (name, value) => {
+        this.props.homeCallback(name, value);
+    }
+    onHandleButtonClick = (name) => {
+        if (name === "SUBMIT") {
+            this.props.submitCallBack();
+        }
+    }
+    onModalClick = (modalType, modalName, value) => {
+        if (modalType === "openModal") {
+            this.props.openModal(modalName, value);
+        }
+
+    }
+    render() {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }}>
+                <h1> Bill Pay Page</h1>
+            </div>
+
+
+        );
+    }
+}
+
+export default BillPay;
